ugilio/ghost

commit 3b16948c4b9704369d8be379a76a80a46fa5b078

Fix regression introduced by 3bc3fc3476ace909566d643df4a084aa88b8f99d

it.cnr.istc.ghost/src/it/cnr/istc/ghost/naming/GhostQualifiedNameProvider.xtend
@@ -14,6 +14,7 @@ import it.cnr.istc.ghost.ghost.FormalParList
 import it.cnr.istc.ghost.ghost.BindList
 import it.cnr.istc.ghost.ghost.ProblemDecl
 import it.cnr.istc.ghost.ghost.EnumLiteral
+import it.cnr.istc.ghost.ghost.NameOnlyParList
 
 class GhostQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider {
 
@@ -35,6 +36,7 @@ class GhostQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider
 	
 	private def isInArgList(EObject obj) {
 		return EcoreUtil2.getContainerOfType(obj,ArgList) !== null
+			|| EcoreUtil2.getContainerOfType(obj,NameOnlyParList) !== null
 			|| EcoreUtil2.getContainerOfType(obj,FormalParList) !== null
 			|| EcoreUtil2.getContainerOfType(obj,BindList) !== null
 	}

commit cf2655593d88de0560c1ef1ceae8611dabb1f328

Added test for regression probably introduced by 3b16948c4b9704369d8be379a76a80a46fa5b078

it.cnr.istc.ghost.tests/src/it/cnr/istc/ghost/tests/GhostCrossRefTest.xtend
@@ -1,0 +1,46 @@
+/*
+ * generated by Xtext 2.12.0
+ */
+package it.cnr.istc.ghost.tests
+
+import com.google.inject.Inject
+import it.cnr.istc.ghost.ghost.Ghost
+import org.eclipse.xtext.testing.InjectWith
+import org.eclipse.xtext.testing.XtextRunner
+import org.eclipse.xtext.testing.util.ParseHelper
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import static org.junit.Assert.*
+import static org.hamcrest.CoreMatchers.*
+import org.eclipse.xtext.EcoreUtil2
+import it.cnr.istc.ghost.ghost.QualifInstVal
+import it.cnr.istc.ghost.ghost.NamedPar
+
+@RunWith(XtextRunner)
+@InjectWith(GhostInjectorProvider)
+class GhostCrossRefTest{
+
+	@Inject
+	ParseHelper<Ghost> parseHelper
+	
+	@Test
+	def void testLocalDom() {
+		val result = parseHelper.parse('''
+type ANumType = int [0,100];
+
+comp ASVWithAnonymousType : sv(
+	A(ANumType x) -> B;
+	B
+synchronize:
+	A(x) -> x < 10;
+);		''')
+		assertNotNull(result);
+		EcoreUtil2.resolveAll(result);
+		val value = EcoreUtil2.eAllOfType(result,QualifInstVal).head?.value;
+		assertNotNull(value);
+		assertThat(value.eIsProxy,is(false));
+		val par = EcoreUtil2.eAllOfType(result,NamedPar).head;
+		assertThat(value,is(par));
+	}
+}