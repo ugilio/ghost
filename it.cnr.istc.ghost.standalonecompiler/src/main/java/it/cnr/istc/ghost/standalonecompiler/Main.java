91f6186fba012ebd8c303affae0c6fa8cbebb077
Giulio Bernardi <ugilio@gmail.com>
2017-11-09 22:00:21 +0000

    Added skeleton of standalone compiler

---
+++ it.cnr.istc.ghost.standalonecompiler/src/main/java/it/cnr/istc/ghost/standalonecompiler/Main.java
@@ -0,0 +1,47 @@
+package it.cnr.istc.ghost.standalonecompiler;
+
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.diagnostics.Severity;
+import org.eclipse.xtext.generator.GeneratorDelegate;
+import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
+
+import com.google.inject.Injector;
+
+import it.cnr.istc.ghost.GhostStandaloneSetup;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		String fname = args[0];
+		
+		Injector injector = new GhostStandaloneSetup().createInjectorAndDoEMFRegistration();
+		XtextResourceSet rs = injector.getInstance(XtextResourceSet.class);
+		
+		XtextResource resource = (XtextResource)rs.getResource(URI.createFileURI(fname),true);
+		//add dependent files...
+		
+		IResourceValidator validator = 
+				resource.getResourceServiceProvider().getResourceValidator();
+		List<Issue> issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+		for (Issue issue : issues)
+			System.out.println(issue.getMessage());
+		
+		if (!issues.stream().anyMatch(p -> p.getSeverity()==Severity.ERROR))
+		{
+			GeneratorDelegate generator = injector.getInstance(GeneratorDelegate.class);
+			JavaIoFileSystemAccess fsa = injector.getInstance(JavaIoFileSystemAccess.class);
+			fsa.setOutputPath(".");
+			generator.doGenerate(resource, fsa);
+		}
+		
+	}
+
+}
