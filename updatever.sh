a1e230fe9dd07a035146c5052b012bf1fc7b3333
Giulio Bernardi <ugilio@gmail.com>
2017-11-29 22:31:34 +0000

    Added script to update version information

---
+++ updatever.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PATTERN='String GHOSTC_VERSION = ';
+VERFILE="com.github.ugilio.ghost.standalonecompiler/src/main/java/com/github/ugilio/ghost/standalonecompiler/Main.java"
+
+NEWVER="$1"
+OLDVER=`egrep "$PATTERN" "$VERFILE" | perl -p -e "s/^.*$PATTERN\"([^\"]+)\".*$/\1/"`
+OLDQVER=`echo $(echo $OLDVER | perl -p -e 's/-.*$//').qualifier`
+NEWQVER=`echo $(echo $NEWVER | perl -p -e 's/-.*$//').qualifier`
+
+if [ "z$NEWVER" == "z" ]
+then
+  echo "Usage: $0 <newversion>" > /dev/stderr
+  exit 1
+fi
+
+if [ "z$OLDVER" == "z" ]
+then
+  echo "Cannot find old version" > /dev/stderr
+  exit 1
+fi
+
+echo "Updating version from $OLDVER to $NEWVER..."
+
+perl -p -i -e "s/$OLDVER/$NEWVER/g" `find com.github.ugilio* -type f -name pom.xml` $VERFILE
+
+if [ "$OLDQVER" != "$NEWQVER" ]
+then
+  perl -p -i -e "s/(\s*Bundle-Version:\s*)$OLDQVER/\${1}$NEWQVER/g" `find com.github.ugilio* -type f -name MANIFEST.MF`
+  perl -p -i -e "s/$OLDQVER/$NEWQVER/" com.github.ugilio.ghost.sdk/feature.xml 
+fi
